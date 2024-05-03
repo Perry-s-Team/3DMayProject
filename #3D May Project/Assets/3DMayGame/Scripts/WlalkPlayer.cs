c57fd2d0126909e77f5876e5070bbe0418a34a0a
9ikii5ec <niiitakirkizh@gmail.com>
2024-05-03 14:24:50 +0300

    Player

--- #3D May Project/Assets/3DMayGame/Scripts/WlalkPlayer.cs
+++ #3D May Project/Assets/3DMayGame/Scripts/WlalkPlayer.cs
@@ -1,19 +1,20 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace DefaultNameSpace {
+
     public class WlalkPlayer : MonoBehaviour
     {
-        [SerializeField] private float speed;
-        private Rigidbody _rb;
+        [SerializeField] private float moveSpeed = 5f; // Скорость перемещения персонажа
 
-        private void Start()
-        {
-            _rb = GetComponent<Rigidbody>();
-        }
         private void Update()
         {
-            Vector3 moveInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            _rb.velocity = moveInput * speed;
+            float moveVertical = Input.GetAxis("Horizontal"); // Получаем значение горизонтальной оси (A и D или стрелки влево и вправо)
+            float moveHorizontal = -1f; // Устанавливаем значение вертикальной оси на постоянное движение назад
+
+            Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical); // Создаем вектор движения на основе полученных значений осей
+            Vector3 newPosition = transform.position + movement * moveSpeed * Time.deltaTime; // Вычисляем новую позицию персонажа
+
+            transform.position = newPosition; // Применяем новую позицию к персонажу
         }
     }
 }
