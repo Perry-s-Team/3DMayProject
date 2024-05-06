5583ca779d858a0e9bb177fa085f2c140f43b77a
9ikii5ec <niiitakirkizh@gmail.com>
2024-05-06 19:49:44 +0300

    BrokenAutoGenerate

---
+++ #3D May Project/Assets/3DMayGame/Scripts/TileGenerator.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileGenerator : MonoBehaviour
+{
+    public GameObject[] tilePrefabs;
+    private List<GameObject> activeTiles = new List<GameObject>();
+    private float spawnPos = 0;
+    private float tileLength = 100;
+
+
+    [SerializeField] private Transform player;
+    private int startTiles = 3;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        for (int i = 0; i < startTiles; i++)
+        {
+            SpawnTile(Random.Range(0, tilePrefabs.Length));
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (player.position.x - 60 > spawnPos - (startTiles * tileLength))
+        {
+            SpawnTile(Random.Range(0, tilePrefabs.Length));
+            DeleteTile();
+        }
+    }
+
+    private void SpawnTile(int tileIndex)
+    {
+        GameObject nextTile = Instantiate(tilePrefabs[tileIndex], new Vector3(spawnPos, 0f, 0f), transform.rotation);
+        activeTiles.Add(nextTile);
+        spawnPos += tileLength;
+    }
+    private void DeleteTile()
+    {
+        Destroy(activeTiles[0]);
+        activeTiles.RemoveAt(0);
+    }
+
+}
--- #3D May Project/Assets/3DMayGame/Scripts/WlalkPlayer.cs
+++ #3D May Project/Assets/3DMayGame/Scripts/WlalkPlayer.cs
@@ -6,43 +6,32 @@ namespace DefaultNameSpace
     [RequireComponent(typeof(Rigidbody))]
     public class WlalkPlayer : MonoBehaviour
     {
-        [SerializeField] private float moveSpeed = 80f; // Скорость перемещения персонажа
+        public float speed = 80f;
 
-        private void Update()
+    void Update()
+    {
+        float horizontalInput = 0f;
+        float verticalInput = 0f;
+
+        if (Input.GetKey(KeyCode.S))
         {
-            float moveVertical = Input.GetAxis("Horizontal"); // Получаем значение горизонтальной оси (A и D или стрелки влево и вправо)
-            float moveHorizontal = -1f; // Устанавливаем значение вертикальной оси на постоянное движение назад
-
-            Vector3 vector31 = new(moveHorizontal, 0f, moveVertical);
-            Vector3 vector3 = vector31;
-            Vector3 movement = vector3; // Создаем вектор движения на основе полученных значений осей
-            Vector3 newPosition = transform.position + moveSpeed * Time.deltaTime * movement; // Вычисляем новую позицию персонажа
-
-            transform.position = newPosition; // Применяем новую позицию к персонажу
+            verticalInput = 1f;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            verticalInput = -1f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            horizontalInput = -1f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            horizontalInput = 1f;
         }
 
-        //[SerializeField] private float moveSpeed = 40f; // Скорость перемещения персонажа
-        //[SerializeField] private GameObject objectA;
-        //[SerializeField] private GameObject objectB;
-        //[SerializeField] private GameObject objectC;
-        //private void FixedUpdate()
-        //{
-        //    float moveVertical = Input.GetAxis("Horizontal"); // Устанавливаем значение вертикальной оси на постоянное движение назад
-        //    float moveHorizontal = -1; // Получаем значение горизонтальной оси (A и D или стрелки влево и вправо)
-
-        //    Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical); // Создаем вектор движения на основе полученных значений осей
-        //    Vector3 newPosition = transform.position + movement * moveSpeed * Time.deltaTime; // Вычисляем новую позицию персонажа
-
-        //    transform.position = newPosition; // Применяем новую позицию к персонажу
-        //}
-
-        //public void OnCollisionEnter(Collision collision)
-        //{
-        //    objectA = collision.gameObject; // Получаем первый объект столкновения
-        //    objectB = collision.gameObject; // Получаем первый объект столкновения
-        //    objectC = this.gameObject;
-        //    Debug.Log("Столкновение между " + objectA.name + " и " + objectB.name);
-        //}
-
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * speed * Time.deltaTime;
+        transform.Translate(movement, Space.Self);
+    }
     }
 }
