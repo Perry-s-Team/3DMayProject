15b182608c2dbc27e8c1187967740a8662be71e0
9ikii5ec <niiitakirkizh@gmail.com>
2024-05-14 17:53:10 +0300

    TileGenerator + Zombie + Ammo

---
+++ #3D May Project/Assets/3DMayGame/Scripts/Z_Follow.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Z_Follow : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private Transform player;
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+    void FixedUpdate()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.position, speed);
+    }
+}
+
