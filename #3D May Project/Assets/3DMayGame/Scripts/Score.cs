9af95609432c2d6b54ff27a2ed289e7c039eebfd
Alex <144246986+alexsamus19@users.noreply.github.com>
2024-05-23 21:11:45 +0300

    storeXxX

--- #3D May Project/Assets/3DMayGame/Scripts/Score.cs
+++ #3D May Project/Assets/3DMayGame/Scripts/Score.cs
@@ -1,17 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Score : MonoBehaviour
+public class Score : PlayerHealth
 {
     [SerializeField] private Transform player;
     [SerializeField] private Text scoreText;
+    [SerializeField] private Text money;
+    [SerializeField] private GameObject panel;
+    [SerializeField] private PlayerHealth health;
 
+    private void Start()
+    {
+        health = GetComponent<PlayerHealth>();
+    }
 
     private void Update()
     {
         if (player != null)
         scoreText.text = ((int)(player.position.z)).ToString();
+
+        if (health.currentHealth <= 0)
+        {
+            //money.text = scoreText.text / 50;
+            panel.SetActive(true);
+        }
     }
 }
